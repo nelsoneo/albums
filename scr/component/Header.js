22e3713b6c82439d8ee969c9a74ebde6da49d134
nelsoneo <nelsoneo@gmail.com>
2021-03-28 11:21:43 -0300

    new test

--- scr/component/Header.js
+++ scr/component/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
-const Header = () => {
-    return <Text>Albums</Text>
-}; 
+const Header = () => <Text style={styles.textHead}>Albums</Text>; 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textHead: {
+        fontSize: 10
+    }
+});
 
 export default Header;
