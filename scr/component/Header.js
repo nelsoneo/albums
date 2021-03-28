2a3124ece55fb2f70ecfe76842e3f4a0c1cce5f6
nelsoneo <nelsoneo@gmail.com>
2021-03-28 11:05:33 -0300

    todos

--- scr/component/Header.js
+++ scr/component/Header.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+const Header = () => {
+    return <Text>Albums</Text>
+} 
+
+const styles = StyleSheet.create({});
+
+export default Header;
